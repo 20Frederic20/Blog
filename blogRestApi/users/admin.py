761aa2b59c647167968838415c30fc7e40a0d72c
20Frederic20 <fredericpierrothountinme@gmail.com>
2023-08-24 14:52:39 +0100

    admin page

--- blogRestApi/users/admin.py
+++ blogRestApi/users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.contrib import admin
 from .models import User, Eleve
+from classes.models import Devoir, Note, Classe, Matiere, Promotion
 
 admin.site.register(User)
 admin.site.register(Eleve)
+admin.site.register(Devoir)
+admin.site.register(Note)
+admin.site.register(Classe)
+admin.site.register(Matiere)
+admin.site.register(Promotion)
