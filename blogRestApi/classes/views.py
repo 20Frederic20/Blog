b03368bbed8d2ae4d4be4bddebc2262ecedd0093
20Frederic20 <fredericpierrothountineme@gmail.com>
2024-04-04 18:54:18 +0100

    calculate and createorupdatenote view

--- blogRestApi/classes/views.py
+++ blogRestApi/classes/views.py
@@ -1,8 +1,13 @@
 from .models import Classe, Promotion, Trimestre, Devoir, Matiere, Coefficient, Filiere
 from users.models import Note
-from rest_framework import viewsets
-from rest_framework import permissions
+from users.models import User, Eleve
 from .serializers import ClasseSerializer, PromotionSerializer, TrimestreSerializer, DevoirSerializer, MatiereSerializer, NoteSerializer, CoefficientSerializer, FiliereSerializer
+
+from rest_framework import viewsets, status, response
+import json
+from rest_framework import permissions
+from rest_framework.views import APIView
+
 
 
 class ClasseViewSet(viewsets.ModelViewSet):
@@ -54,18 +59,17 @@ class MatiereViewSet(viewsets.ModelViewSet):
     #     return Matiere.objects.all()  # Renvoyer une liste vide si aucun ID de classe n'est fourni
     
     def get_queryset(self):
-        eleve = self.request.GET.get('eleve', None)
-        if eleve:
-            coefficients = Coefficient.objects.filter(classe=eleve.classe, filiere=eleve.filiere)
+        matieres_representation = []
+        filiere_id = self.request.GET.get('filiere_id', None)
+        classe_id = self.request.GET.get('classe_id', None)
+        
+        if filiere_id and classe_id:
+            coefficients = Coefficient.objects.filter(classe__id=classe_id, filiere__id=filiere_id)
             if coefficients:
-                matieres_representation = []
                 for coefficient in coefficients:
-                    matiere_representation = {
-                        'id': coefficient.matiere.id,
-                        'codeMatiere': coefficient.matiere.codeMatiere,
-                        'denomination': coefficient.matiere.denomination,
-                    }
-                    matieres_representation.append(matiere_representation)
+                    matiere = Matiere.objects.filter(id=coefficient.matiere.id).first()
+                    matieres_representation.append(matiere)
+        if matieres_representation:
             return matieres_representation
         return Matiere.objects.all()
 
@@ -92,3 +96,71 @@ class FiliereViewSet(viewsets.ModelViewSet):
     """
     queryset = Filiere.objects.all()
     serializer_class = FiliereSerializer
+    
+
+class CalculationsView(APIView):
+    def post(self, request, eleve_id, format=None):
+        try:
+            eleve = Eleve.objects.get(id=eleve_id)
+            coefficient = Coefficient.objects.filter(classe=eleve.classe, filiere=eleve.filiere)
+            coefficient_value, notes_interrogation, notes_devoir = [], [], []
+            
+            if coefficient:
+                for coef in coefficient:
+                    tableau = []  # Réinitialiser la liste à chaque itération
+                    notes = Note.objects.filter(eleve=eleve, matiere=coef.matiere)
+                    if notes.exists():
+                        notes_interrogation = [note.valeur for note in notes if "Interrogation" in note.devoir.denomination]
+                        notes_devoir = [note.valeur for note in notes if "Devoir" in note.devoir.denomination]# Vérifier s'il y a des notes associées
+                        for note in notes:
+                            petit = {
+                                'note': note.valeur,
+                                'devoir': note.devoir.denomination,
+                            }
+                            tableau.append(petit)
+                        notes_matieres = {
+                            'notes': tableau,
+                            'coefficient_value': coef.valeur,
+                            'matiere': coef.matiere.denomination
+                        }
+                        coefficient_value.append(notes_matieres) 
+            else:
+                coefficient_value = 1 # Valeur par défaut si aucun coefficient n'est trouvé
+            moyenne_interro = sum(notes_interrogation) / len(notes_interrogation)
+            moyenne_devoir = sum(notes_devoir) / len(notes_devoir)
+
+            return response.Response(
+                {
+                    'coefficient': coefficient_value, 
+                    'interrogations':notes_interrogation, 
+                    'devoirs': notes_devoir, 
+                    'moyenne_interro': moyenne_interro, 
+                    'moyenne_devoir': moyenne_devoir
+                }, status=status.HTTP_200_OK)
+        except Eleve.DoesNotExist:
+            return response.Response({'error': 'Eleve not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+
+class CreateorUpdateNoteView(APIView):
+    def post(self, request, *args, **kwargs):
+        # matiere = self.request.POST.get('matiere', None)
+        # eleve = self.request.POST.get('eleve', None)
+        # trimestre = self.request.POST.get('trimestre', None)
+        # devoir = self.request.POST.get('devoir', None)
+        # note = self.request.POST.get('note', None)
+        
+        try:
+        
+            print(request.POST)
+            nouvellesNotes =[] 
+            notes = []
+            if nouvellesNotes:
+                # nouvellesNotes = json.loads(nouvellesNotes)
+                for note in nouvellesNotes:
+                    verified = Note.objects.filter(matiere=note['matiere'], devoir=note['devoir'], trimestre=note['trimestre'], eleve=note['eleve'])
+                    if not verified:
+                        note_data = Note.objects.create(matiere=note['matiere'], devoir=note['devoir'], trimestre=note['trimestre'], eleve=note['eleve'], valeur=note['valeur'])
+                        notes.append(note_data)
+            return response.Response({'message': 'Notes ajoutés avec succès', 'notes': nouvellesNotes}, status=status.HTTP_201_CREATED)
+        except:
+            return response.Response({'message': 'Erreur'}, status=status.HTTP_400_BAD_REQUEST)
