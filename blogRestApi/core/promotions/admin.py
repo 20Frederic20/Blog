9582e837eaeebaa0c2cd156793d3fcd8f764565b
20Frederic20 <fredericpierrothountineme@gmail.com>
2024-04-25 03:12:18 +0100

    admin files

--- blogRestApi/core/promotions/admin.py
+++ blogRestApi/core/promotions/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import User, Student, Teacher, Promotion, Classroom, Course, Coefficient
+from .models import User, Student, Teacher, Promotion, Classroom, Course, Coefficient, Subject, Register, Instruct, Score
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Teacher)
+admin.site.register(Promotion)
+admin.site.register(Classroom)
+admin.site.register(Course)
+admin.site.register(Coefficient)
+admin.site.register(Register)
+admin.site.register(Instruct)
+admin.site.register(Score)
